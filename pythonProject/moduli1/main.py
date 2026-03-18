6b7c492d4e1bebaf66dead26e7b603eaa17a6ec8
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-03-18 20:37:40 +0100

    moduli1

---
+++ pythonProject/moduli1/main.py
@@ -0,0 +1,28 @@
+temperature= 98.6
+
+name = "John"
+
+print(type(temperature))
+print(type(name))
+
+age=30
+print(age)
+
+x=5
+y=7
+
+result = x + y
+print(result)
+
+age = age+1
+print(age)
+
+first_name= "John"
+last_name="Doe"
+
+full_name= first_name +" "+last_name
+print(full_name)
+
+name_of_the_list =["item 1","item 2","item 3","item 4",]
+shoping_list =["apples","Bananas", 3,4,5]
+print(shoping_list[1])
