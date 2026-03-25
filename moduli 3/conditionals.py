594205500aa6897777bf3d3cc6414c4f3a582688
Gjin893 <gjinibq@gmail.com>
2026-03-25 20:35:37 +0100

    moduli 3

---
+++ moduli 3/conditionals.py
@@ -0,0 +1,21 @@
+age= 18
+
+if age >=18:
+    print("you can vote.")
+else:
+    print("you cannot vote")
+
+    temp=28
+    if temp>30:
+        print("it's a hot day")
+    elif 20<=temp <=30:
+        print("The weather is pleasent")
+    else:
+        print("it's a cod day")
+
+        number = 7
+
+        if number %2 ==0:
+            print("Numri eshte qift")
+        else:
+            print("Numri eshte tek")
