f183fd73b46510bdd4c47690d60e4aab0ffc76c2
Gjin893 <gjinibq@gmail.com>
2026-04-15 20:38:20 +0200

    modul 6 dhe 7

---
+++ moduli 6/packages/index.py
@@ -0,0 +1,5 @@
+from my_packages import modul1 as m1
+from my_packages import modul2 as m2
+
+m1.hello()
+m2.hello()
---
+++ moduli 7/input_casting.py
@@ -0,0 +1,9 @@
+number1 = int (input("Enter the first number:"))
+number2 = int (input("Enter the second  number:"))
+
+result = number1 + number2
+
+print(f"the sum of {number1} and {number2} is {result}")
+
+
+
