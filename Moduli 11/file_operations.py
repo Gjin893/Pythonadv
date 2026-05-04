5dd1dd519428905c964f26563afbcc4fba0bdc9d
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-05-04 20:33:33 +0200

    moduli 11

---
+++ Moduli 11/file_operations.py
@@ -0,0 +1,54 @@
+#Opening and closing the file
+# file_path = "example.txt"
+# file = open(file_path,"r")
+#
+# content = file.read()
+# print(content)
+#
+# file.close()
+
+import os
+
+with open("example.txt","r") as file:
+    content = file.read()
+    print(content)
+    line = file.readline()
+    print(line)
+    lines = file.readlines()
+    print(lines)
+
+with open("example.txt","w") as file:
+    file.write("Pershendetje Orges!")
+
+
+lines = ['Hello Wolrd\n','Welcome to Python\n']
+with open("example.txt","w") as file:
+    file.writelines(lines)
+
+
+with open("example.txt","r") as file:
+    file.seek(0)
+    data = file.read()
+    print(data)
+
+if os.path.exists('example.txt'):
+    print("File exist")
+
+
+data = b'This is some binary date'
+with open("example.bin", 'wb') as file:
+    file.write(data)
+
+
+with open("example.bin", 'rb') as binary_file:
+    data = binary_file.read()
+    print(data)
+
+
+
+
+
+
+
+
+
